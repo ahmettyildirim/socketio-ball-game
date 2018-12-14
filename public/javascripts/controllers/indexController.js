719e3a3604b9053b129217bd84e9359519ce2e55
AHMET <ahmetyildirim3@gmail.com>
2018-12-14 22:48:20 +0300

    take username from prompt and save it users array in socket.io

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -1,10 +1,23 @@
 app.controller('indexController',['$scope','indexFactory', ($scope, indexFactory) => {
-    indexFactory.connectSocket('http://localhost:3000', {
-        reconnectionAttempts :3 ,
-        reconnectionDelay: 600
-    }).then((socket) => {
-        console.log('connected successfully',socket);
-    }).catch((err) => {
-        console.log(err);
-    });
+    
+    $scope.init = () => {
+        const username = prompt('Please enter username');
+
+        if(username)
+            initSocket(username);
+        else
+            return false;
+    }
+    
+    
+    function initSocket(username){
+        indexFactory.connectSocket('http://localhost:3000', {
+            reconnectionAttempts :3 ,
+            reconnectionDelay: 600
+        }).then((socket) => {
+            socket.emit('newUser', {username})
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -2,10 +2,24 @@ const socketio = require('socket.io');
 const io = socketio();
 
 const socketApi = { };
-
 socketApi.io = io;
+
+const users = [ ];
+
 io.on('connection', (socket) => {
     console.log('user connected');
+        socket.on('newUser', (data) =>{
+            const userData = {
+                username : data.username,
+                id: socket.id,
+                position: {
+                    x: 0,
+                    y:0 
+                }
+            }
+            users.push(userData);
+            console.log(users);
+        });
 });
 
 module.exports = socketApi;
